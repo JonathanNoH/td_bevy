547a2cc537858b40e1544f2548b372300771ed9d
Jonathan Holst <97708227+JonathanNoH@users.noreply.github.com>
2024-05-22 22:08:59 -0600

    spawn a player sprite to center screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    let texture: Handle<Image>  = asset_server.load("disciple-45x51.png");
+    let layout = TextureAtlasLayout::from_grid(Vec2::new(45.,51.), 4, 3, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(SpriteSheetBundle {
+        texture,
+        atlas: TextureAtlas {
+            layout: texture_atlas_layout,
+            ..default()
+        },
+        ..default()
+    });
 }
